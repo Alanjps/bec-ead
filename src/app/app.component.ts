2aeb874a8bcb2f5253f6f58934b9ebaf73a6ce93
Alanjps <alanjuliano.ps@hotmail.com>
2021-08-09 09:10:14 -0300

    teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,7 @@ export class MyApp {
         //12 -> sehc
         //13 -> club-learn
         
-        this.projectNumber = 5;
+        this.projectNumber = 2;
 
         switch(this.projectNumber){
           case 0:
