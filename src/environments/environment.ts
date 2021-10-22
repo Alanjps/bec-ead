5cc9930e7f8d63bc5cf14db42e0d7f8053f4c2db
Alanjps <alanjuliano.ps@hotmail.com>
2021-10-22 17:12:12 -0300

    Club Learn

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export var environment = {
     //style-orange
 
 
-
+/* 
  //KNOW-HOUSE -----------------------------------
   PROJECT: 'know-house',
   TEXT_COLOR_V1: 'text-default-black',  
@@ -20,7 +20,7 @@ export var environment = {
   PUSHER_APP_KEY: "25af961ea9f2ef86b9aa",
   PUSHER_APP_SECRET: "c6060d1a53114a8365da",
   PUSHER_APP_CLUSTER: "us2", 
-
+ */
   
 /* 
 //EDICOM -----------------------------------
@@ -146,7 +146,7 @@ PUSHER_APP_CLUSTER: "us2",
 
  */
 
-/* 
+
 //CLUB LEARN -----------------------------------
 PROJECT: 'club-learn',
 TEXT_COLOR_V1: 'text-default-black',  
@@ -156,7 +156,7 @@ PUSHER_APP_ID: "1193372",
 PUSHER_APP_KEY: "5a5d9d9d6e878a5a139e",
 PUSHER_APP_SECRET: "cfc406609b91179a3e40",
 PUSHER_APP_CLUSTER: "us2",
- */
+
 
 /* 
 //HKT-369 -----------------------------------
